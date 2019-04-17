408cbbf008ecf843fe3e7ff7fea335fa870ffacc
Jon Crossley <joncrossley@lyft.com>
2019-04-17 10:49:49 -0700

    [Git] Add gitignore_global and git-completion

---
+++ git/git-completion.bash
@@ -0,0 +1,8 @@
+# If needed, brew install git bash-completion
+
+[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion || {
+    # if not found in /usr/local/etc, try the brew --prefix location
+    [ -f "$(brew --prefix)/etc/bash_completion.d/git-completion.bash" ] && \
+        . $(brew --prefix)/etc/bash_completion.d/git-completion.bash
+}
+
