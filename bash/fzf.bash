c5863963aee93ff64112c69986137d79e6025d5a
Jon Crossley <joncrossley@lyft.com>
2019-07-08 12:40:41 -0700

    Add fzf

---
+++ bash/fzf.bash
@@ -0,0 +1,17 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/local/opt/fzf/shell/key-bindings.bash"
+
+# Customizations
+# --------------
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
